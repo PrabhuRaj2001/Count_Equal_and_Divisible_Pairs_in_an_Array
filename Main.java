cae258977fc0ebcede91a08799b589a09bc04f6f
PrabhuRaj2001 <prabhuraj152001@gmail.com>
2023-06-19 09:20:21 +0530

    added success fully

---
+++ Main.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countPairs(int[] a, int k) {
+			int count=0;
+        	for(int i=0;i<a.length;i++)
+		{
+			for(int j=i+1;j<a.length;j++)
+			{
+				if(a[i]==a[j])
+				{
+					if((i*j)%k==0)
+					{
+						count++;
+					}
+				}
+			}
+		
+        }
+       return count;
+    }
+}
